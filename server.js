44d80bf0727d1c92fed261715c535289cdb4257e
Soumya Tanwar <soumyatanwar@users.noreply.github.com>
2019-08-06 23:41:25 +0530

    Music Mixer

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express')
-const path = require('path')
+const http = require('http')
+const socketio = require('socket.io')
 
+// The Express App
 const app = express()
+// The server on which the app runs
+const server = http.Server(app)
+// The socket.io server
+const io = socketio(server)
 
 // For security reasons, heroku does not allow you to set the port, trying to use 3333 would have led
 // to a communication timeout error, so we need to allow the PORT specified in the env of the OS of 
 // the machine that Heroku will use to host the application
-const SERVER_PORT = process.env.PORT || 3333
+const PORT = process.env.PORT || 2323
 
-app.use('/public', express.static(path.join(__dirname, 'public')))
+io.on('connection', function(socket){
+    console.log('Socket created: '+ socket.id)
+    socket.on('msg', function(data){
+        socket.broadcast.emit('msg', {
+            sender: users[socket.id],
+            message: data.message
+        })
+    })
+    socket.on('login', function(data){
+        users[socket.id] = data.username
+        socket.emit('logged_in')
+    })
+    socket.on('play', function(data){
+        io.emit('play',data)
+    })
+})
 
-app.get('/', (req, res) => res.send("Hello from the BackEnd"))
+app.use('/', express.static(__dirname + '/public'))
 
-app.listen(SERVER_PORT, () => console.log("Server started"))
+server.listen(PORT, () =>{
+    console.log('Server started at http://localhost:${PORT}')
+})
